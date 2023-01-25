9d1ebce5fdc2de7e94c280a2e4cc84deec0e3c7d
Alexis <lexisgasa3007@gmail.com>
2023-01-25 17:05:13 +0100

    funcion getEmail implementada

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,41 +1,13 @@
 /* Crea tu propia lógica para hacer fetch de 3 posts distintos y enseñarlos en la homepage con DOM manipulation */
 
+
+// get in the console the value input from the support form 
+
 let getEmail = (e) => {
     e.preventDefault();
     let email = document.querySelector("#email").value;
-    fetch("contact.json", {
-        method: "POST",
-        body: JSON.stringify({email:"email"}), 
-        headers: {
-            "Content-type": "application/json; charset=UFT-8",
-        },
-    })
-    .then((response) => response.json())
-    .then((json) => console.log(json) )
-    .catch((error) => console.log(error))
+    console.log(email)
 };
 
-document.querySelector("#getSupport").addEventListener("submit", console.log("test"))
+document.querySelector("#getSupport").addEventListener("submit", getEmail)
 
-
-
-
-// let getEmail2 = (e) => {
-//     e.preventDefault();
-//     let email = document.querySelector("#email").value;
-//     fetch('https://jsonplaceholder.typicode.com/posts', {
-//         method: 'POST',
-//         body: JSON.stringify({
-//             email: 'foo',
-//             body: 'bar',
-//             userId: 1,
-//         }),
-//         headers: {
-//             'Content-type': 'application/json; charset=UTF-8',
-//         },
-//     })
-//     .then((response) => response.json())
-//     .then((json) => console.log(json) )
-//     .catch((error) => console.log(error))
-// }
-// document.querySelector("#getSupport").addEventListener("submit", getEmail2)
