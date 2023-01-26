e7e27bb69e53b8ad1064a75cba0d15e47b98575b
Alexis <lexisgasa3007@gmail.com>
2023-01-26 18:24:32 +0100

    hamburguesa hecha

--- resources/js/index.js
+++ resources/js/index.js
@@ -11,3 +11,18 @@ let getEmail = (e) => {
 
 document.querySelector("#getSupport").addEventListener("submit", getEmail)
 
+
+// hamburguer animation
+
+const hamburger = document.querySelector(".hamburger")
+const navLinks = document.querySelector(".nav-links")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navLinks.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    navLinks.classList.remove("active")
+}))
